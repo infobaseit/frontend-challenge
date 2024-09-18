9beabd4e4ba2bcb23cbb2d041106fd699ac2fb1a
Leandro Carmo <leandrocarmo2012@gmail.com>
2024-09-17 23:50:21 -0300

    modulo-hq-marvels

--- app/code/Infohq/Comics/Helper/Data.php
+++ app/code/Infohq/Comics/Helper/Data.php
@@ -8,6 +8,11 @@ class Data extends AbstractHelper
     protected $publicKey = 'ed1071b6f15d6c749ff1216fe4400a6e';
     protected $privateKey = '97e6e0bae037dec89e580960db49b9a07c709642';
 
+    /**
+     * Retrieves a list of comics from the Marvel API.
+     *
+     * @return array List of comics
+    */
     public function getComics()
     {
         $timestamp = time();
@@ -22,6 +27,13 @@ class Data extends AbstractHelper
         $data = json_decode($response, true);
         return isset($data['data']['results']) ? $data['data']['results'] : [];
     }
+
+    /**
+     * Retrieves the details of a specific comic by ID from the Marvel API.
+     *
+     * @param int $comicId The ID of the comic
+     * @return array|null Details of the comic or null if not found
+    */
 
     public function getComicDetails($comicId)
     {
