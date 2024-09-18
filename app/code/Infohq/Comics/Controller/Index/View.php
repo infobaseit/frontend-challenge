38485a8d239f0ea0663799072788f660ab46a9fc
Leandro Carmo <leandrocarmo2012@gmail.com>
2024-09-17 23:11:11 -0300

    modulo-hq-marvels

---
+++ app/code/Infohq/Comics/Controller/Index/View.php
@@ -0,0 +1,23 @@
+<?php
+namespace Infohq\Comics\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class View extends Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(Context $context, PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $page = $this->resultPageFactory->create();
+        return $page;
+    }
+}
---
+++ app/code/Infohq/Comics/Helper/Data.php
@@ -0,0 +1,40 @@
+<?php
+namespace Infohq\Comics\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+
+class Data extends AbstractHelper
+{
+    protected $publicKey = 'ed1071b6f15d6c749ff1216fe4400a6e';
+    protected $privateKey = '97e6e0bae037dec89e580960db49b9a07c709642';
+
+    public function getComics()
+    {
+        $timestamp = time();
+        $hash = md5($timestamp . $this->privateKey . $this->publicKey);
+        $comicsUrl = "https://gateway.marvel.com/v1/public/comics?ts={$timestamp}&apikey={$this->publicKey}&hash={$hash}&limit=100";
+
+        $response = @file_get_contents($comicsUrl);
+        if ($response === FALSE) {
+            return [];
+        }
+
+        $data = json_decode($response, true);
+        return isset($data['data']['results']) ? $data['data']['results'] : [];
+    }
+
+    public function getComicDetails($comicId)
+    {
+        $timestamp = time();
+        $hash = md5($timestamp . $this->privateKey . $this->publicKey);
+        $comicUrl = "https://gateway.marvel.com/v1/public/comics/{$comicId}?ts={$timestamp}&apikey={$this->publicKey}&hash={$hash}";
+
+        $response = @file_get_contents($comicUrl);
+        if ($response === FALSE) {
+            return null;
+        }
+
+        $data = json_decode($response, true);
+        return isset($data['data']['results'][0]) ? $data['data']['results'][0] : null;
+    }
+}
